e6b729156e7caa9cc227861e545c7b12120683a3
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2022-10-10 12:10:44 -0500

    first commit

--- main.js
+++ main.js
@@ -1,127 +1,122 @@
-/* Practice Assignment 3:
+/* Practice Assignment 6:
 
 Complete this javascript file according to the individual instructions
 given in the comments. 
 
 *** DO NOT CHANGE any of the code that you are not instructed to. */
 
-// 1) Construct a basic IF statement that assigns "Informatics" to the
-// myDept variable IF the value of myNumber is greater than 1.
-// The variables are already declared.
-// Do not change the existing code.
+// 1) Create an array named myPizzaOrder that cannot be reassigned and
+// contains the 3 elements: "Pepperoni", 12.99, false
+const myPizzaOrder = ["Pepperoni", 12.99, false]
 
-const question1 = (myNumber) => {
-	let myDept
-	/////// write your IF statement below this line /////////
-	if (myNumber > 1) {
-		myDept = Informatics
+// 2) Create a function named getPizzaType that will accept an array
+// as a parameter and returns the 1st element from the array.
+const getPizzaType = (pizzaOrder) => {
+	return pizzaOrder[0]
+}
+
+// 3) Create a function named addTax that will accept an array
+// like your myPizzaOrder array as a parameter. The function will
+// insert an element into the array between the 2nd and 3rd
+// element in the array that is equal to the 2nd element multiplied
+// by 0.0925 and rounded to the nearest penny (ie 0.95, 0.79, 0.42, etc).
+// The function should return the array.
+// ===========
+// Hint: find the method that will allow you to set the decimals.
+const addTax = (pizzaOrder) => {
+	tax = (pizzaOrder[1] * .0925).toFixed(2)
+	pizzaOrder.splice(2, 0, parseFloat(tax))
+	return pizzaOrder
+}
+console.log(addTax(myPizzaOrder))
+
+// 4) Create a function named completeOrder that will accept an array
+// as a parameter. The function will change the value of the 4th
+// element in the array to true and return the array.
+const completeOrder = (pizzaOrder) => {
+	pizzaOrder[3] = true
+	return pizzaOrder
+}
+console.log(completeOrder(myPizzaOrder))
+
+// 5) Create a function named allTheNumbers that accepts a number as
+// a parameter. The function will create an array that contains all of
+// the numbers from 1 to whatever number the parameter is. The array
+// should not include the parameter number. The function returns
+// the array.
+const allTheNumbers = (number)=>{
+	//creates an array from parameter number
+	let numbers = (max)=> Array.from(new Array(max), (_, i)=> i)
+	//filters out 0
+	numbersArray = numbers(number).filter(x=> x!=0)
+
+	return numbersArray
+}
+console.log(allTheNumbers(10))
+
+// 6) Create a function named getEvens that accepts 2 parameters:
+// startNum and endNum. The function should return an array that
+// includes the even numbers from startNum to endNum. If the startNum
+// or endNum are even, they should be included in the returned array.
+const getEvens = (startNum, endNum)=>{
+	evenArray = []
+	for(let i=startNum; i<=endNum; i++){
+		if(i%2===0){
+			evenArray.push(i)
+		}
 	}
-	/////// write your IF statement above this line /////////
-	return myDept
+	return evenArray
 }
 
-// 2) Construct an IF statement that assigns "JavaScript" to the
-// myLang variable IF the length property of myValue is equal to 10.
-// Add an ELSE statement that assigns "Python" otherwise.
-// The variables are already declared.
-// Do not change the existing code.
+console.log(getEvens(2,10))
 
-const question2 = (myValue) => {
-	let myLang
-	/////// write your IF statement below this line /////////
-	if (myValue.length === 10) {
-		myLang = "JavaScript"
-	} else {
-		myLang = "Python"
-	}
-	/////// write your IF statement above this line /////////
-	return myLang
+// 7) Create a function named getLastElement that accepts an array
+// as a parameter and returns the last element of the array. The
+// array can have any number of elements. The function should always
+// return the last element.
+const getLastElement = (theArray)=>{
+	end = theArray.length-1
+	return theArray[end]
+}
+console.log(getLastElement(myPizzaOrder))
+
+// 8) Create a function named combineArrays that accepts two arrays
+// as parameters and combines them into one array. The function
+// should return the new combined array.
+
+const combineArray = (firstArray, secondArray)=>{
+	firstArray.push(...secondArray)
+	return firstArray
 }
 
-// 3) Copy the IF / ELSE statement from #2 and paste it below.
-// Add an ELSE IF statement to it that checks if the length property of
-// myValue is greater than 3. If true, assign "PHP" to myLang.
-// The variables are already declared.
-// Do not change the existing code.
+const numbersList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const question3 = (myValue) => {
-	let myLang
-	/////// write your IF statement below this line /////////
-	if (myValue.length === 10) {
-		myLang = "JavaScript"
-	} else if (myValue.length > 3) {
-		myLang = "PHP"
-	} else {
-		myLang = "Python"
-	}
-	/////// write your IF statement above this line /////////
-	return myLang
+console.log(combineArray(myPizzaOrder, numbersList))
+
+// 9) Create a function named isPalindrome that accepts a string as
+// a parameter. The function will return true if the string is a
+// palindrome and false if the string is not. To check for a
+// palindrome, remove all spaces and set to lowercase. The string
+// will be identical both forward and reverse if it is a palindrome.
+// For example: Taco Cat is a palindrome. tacocat is the same both
+// forward and reverse.
+// ==========
+// Hints: There are several possible solutions for this. I use a
+// combination of string and array methods. You can turn the string
+// into an array, use arrays methods, and turn the array back into
+// a string. This is also a challenge on freeCodeCamp. Check the
+// help forums there if you get stuck!
+
+
+const isPalindrome = (s)=>{
+
+//remove all non alphanumeric characters
+let cleanString = s.toLowerCase().replace(/[\W_]/g,'')
+
+//reverse the string to check
+let reverseCleanString = cleanString.split('').reverse().join('')
+
+return cleanString === reverseCleanString
 }
 
-// 4) Construct a SWITCH statement that will assign the following values to the variable
-// myFaveFood based on the value of myNumber the SWITCH statement receives:
-// 1 = Pizza, 2 = Hamburger, 3 = Ice Cream, 4 = Sushi, Anything else = Pho
-// The variables are already declared.
-// Do not change the existing code.
-
-const question4 = (myNumber) => {
-	let myFaveFood
-	/////// write your SWITCH statement below this line /////////
-    switch (myNumber) {
-        case 1: myFaveFood = "Pizza"
-                break;
-        case 2: myFaveFood = "Hamburger"
-                break;
-        case 3: myFaveFood = "Ice Cream"
-                break;
-        case 4: myFaveFood = "Sushi"
-                break;
-        default:
-                myFaveFood = "Pho"
-    }
-
-	/////// write your SWITCH statement above this line /////////
-	return myFaveFood
-}
-
-// 5) Construct a TERNARY statement with the ternary operator
-// that checks if the variable myTemp is greater than or equal to 75.
-// If true, assign the value "Great weather!" to the variable myWeather.
-// If false, assign the value "Still cold." to the variable myWeather.
-// This ternary operator statement should just be one line of code.
-// The variables are already declared.
-// Do not change the existing code.
-
-const question5 = (myTemp) => {
-	let myWeather
-	/////// write your TERNARY OPERATOR statement below this line /////////
-    myWeather = (myTemp > 75)? "Great weather!": "Still cold."
-	/////// write your TERNARY OPERATOR statement above this line /////////
-	return myWeather
-}
-
-// Practice Assignment 3
-// • Question 1
-// The variable myDept should not have a value if the variable myNumber is not greater than 1.
-// The variable myDept should hold the required value if the variable myNumber is greater than 1.
-// • Question 2
-// The variable myLang should hold the required value if the variable myValue has a length property equal to 10.
-// The variable myLang should hold the required value if the variable myValue does not have a length property
-// equal to 10.
-// • Question 3
-// The variable myLang should hold the required value if the variable myValue has a length property equal to 10.
-// The variable myLang should hold the required value if the variable myValue has a length property greater than
-// 3.
-// The variable myLang should hold the required value if the variable myValue does not have a length property
-// equal to 10.
-// • Question 4
-// The variable myFaveFood should hold the required value if the variable myNumber is equal to 1.
-// The variable myFaveFood should hold the required value if the variable myNumber is equal to 2.
-// The variable myFaveFood should hold the required value if the variable myNumber is equal to 3.
-// The variable myFaveFood should hold the required value if the variable myNumber is equal to 4.
-// The variable myFaveFood should hold the required value if the variable myNumber is anything other than the
-// values 1 through 4.
-// • Question 5
-// The variable myWeather should hold the required value if the variable myTemp has a value greater than 75.
-// The variable myWeather should hold the required value if the variable myTemp has a value equal to 75.
-// The variable myWeather should hold the required value if the variable myTemp has a value less than 75.
+console.log(isPalindrome("A man, a plan, a canal. Panama"))
